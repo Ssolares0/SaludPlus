af29068509aa96145e84825508fbd2e995ab1c31
Jhonsagui81 <3634733270101@ingenieria.usac.edu.gt>
2025-03-17 14:42:32 -0600

    feat: backend connection with database

--- backend/src/config/appServer.ts
+++ backend/src/config/appServer.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import cors from 'cors'
+import { connectPg } from './database/Postgres';
 
+const PORT = 3001;
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.use(
+    cors({
+        origin: '*', 
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+        allowedHeaders: ['Content-Type', 'Authorization'] 
+    })
+)
+
+connectPg();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.status(200).json({ status: 'OK' });
 });
+  
+app.get('/version', (req, res) => {
+res.status(200).json({ version: '1.0.0' });
+});
+  
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });

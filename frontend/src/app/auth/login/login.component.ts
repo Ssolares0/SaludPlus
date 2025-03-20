c91c47f74dd4beb8c95fb8ea3d8492632775c938
JCarpeta <3082203580607@ingenieria.usac.edu.gt>
2025-03-19 23:58:55 -0600

    feat: add login component with styles and functionality

---
+++ frontend/src/app/auth/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent {
+  constructor(private router: Router) {}
+
+  goToSelectTypeAccount() {
+    this.router.navigate(['/select-type-account']);
+  }
+}

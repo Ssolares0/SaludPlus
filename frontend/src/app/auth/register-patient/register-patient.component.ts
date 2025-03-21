7c1a5fad41766e6be93619182b8efc7483d9e0f5
JCarpeta <3082203580607@ingenieria.usac.edu.gt>
2025-03-21 00:02:15 -0600

    feat: add register patient component with HTML, CSS, and functionality

--- frontend/src/app/auth/register-patient/register-patient.component.ts
+++ frontend/src/app/auth/register-patient/register-patient.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { UserCircle2 } from 'lucide-angular';
 
 @Component({
   selector: 'app-register-patient',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './register-patient.component.html',
   styleUrl: './register-patient.component.css'
 })
-export class RegisterPatientComponent {
 
+export class RegisterPatientComponent {
+  constructor() { }
+
+  protected readonly UserCircle2 = UserCircle2;
 }

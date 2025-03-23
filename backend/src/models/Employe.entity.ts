a40cbb82169ce191e82f095a51a5c9ecae9c2631
Jhonsagui81 <3634733270101@ingenieria.usac.edu.gt>
2025-03-23 15:54:01 -0600

    feat: add all models

--- backend/src/models/Employe.entity.ts
+++ backend/src/models/Employe.entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany, OneToOne, JoinColumn
 import { Person } from './Person.entity';
 import { EmployeeSpecialty } from './EmployeeSpecialties.entity';
 import { EmployeeDepartmetn } from './EmployeeDepartment.entity';
+import { Appointment } from './Appointments.entity';
 
 @Entity({name:'employees'})
 export class Employee {
@@ -17,13 +18,17 @@ export class Employee {
   @Column({type:'decimal', nullable:true})
   salary!: number;
 
-  //Relacion Especialidaddes
+  //Relacion Especialidaddes 1:N
   @OneToMany(() => EmployeeSpecialty, (employeeSpecialty) => employeeSpecialty.employee)
   specialty!: EmployeeSpecialty[];
 
-  //Relacion Departamentos
+  //Relacion Departamentos 1:N
   @OneToMany(() => EmployeeDepartmetn, (employeeDepartment) => employeeDepartment.employee)
   department!: EmployeeDepartmetn[];
+
+  //Relacion con Citas 1:N
+  @OneToMany(() => Appointment, (appointment) => appointment.doctor)
+  appointments!: Appointment[];
 
   @OneToOne(() => Person)
   @JoinColumn({name:'person_id'})

526a5fb0fd0c024fdce00644358b49e8377b4a73
JCarpeta <3082203580607@ingenieria.usac.edu.gt>
2025-04-01 08:26:06 -0600

    feat(doctor): add DataDoctorResponse model and implement getDataDoctor service method

---
+++ frontend/src/app/doctor/services/doctor.service.ts
@@ -0,0 +1,38 @@
+import { DataDoctorResponse } from "../models/doctor.model";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { catchError, throwError } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class DoctorService {
+    private readonly baseUrl = 'http://localhost:3001';
+
+    constructor(private http: HttpClient) { }
+
+    public getDataDoctor(id: number): Observable<DataDoctorResponse> {
+        return this.http.get<DataDoctorResponse>(`${this.baseUrl}/employee/doctor/${id}`)
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        let errorMessage = '';
+
+        if (error.error instanceof ErrorEvent) {
+            errorMessage = `Error del lado del cliente: ${error.error.message}`;
+        } else {
+            const serverError = error.error?.error || error.error?.message || error.statusText;
+            errorMessage = `Error del servidor: Código ${error.status}, Mensaje: ${serverError}`;
+
+            console.error('Cuerpo completo del error:', error.error);
+        }
+
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+    }
+}
